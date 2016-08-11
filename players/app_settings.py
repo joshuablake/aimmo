05f06a26a7c76251428251324482a40f88ceab09
Joshua Blake <joshua.blake@ocado.com>
2016-08-11 14:53:29 +0100

    Check aimmo-game-creator authentication in API

--- players/app_settings.py
+++ players/app_settings.py
@@ -7,4 +7,6 @@ GAME_SERVER_LOCATION_FUNCTION = getattr(
     lambda id: ('http://localhost:%d' % (6001 + int(id) * 1000), '/socket.io'),
 )
 
+CREATOR_AUTH_TOKEN = getattr(settings, 'CREATOR_AUTH_TOKEN', 'insecure-creator-auth-token')
+
 MAX_LEVEL = 1
